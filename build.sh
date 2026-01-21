e44c71dc22849ccddf6ae73a889afbeb5a4197da
Subhajit Sahu <wolfram77@gmail.com>
2026-01-21 06:35:54 +0530

    :bug: update build script

--- build.sh
+++ build.sh
@@ -1,46 +1,27 @@
 #!/usr/bin/env bash
 # Fetch the latest version of the library
 fetch() {
-if [ -d "taskflow" ]; then return; fi
-URL="https://github.com/taskflow/taskflow/archive/refs/heads/master.zip"
-ZIP="${URL##*/}"
-DIR="taskflow-master"
-mkdir -p .build
-cd .build
+if [ -f "semver.hpp" ]; then return; fi
+URL="https://github.com/Neargye/semver/raw/refs/heads/master/include/semver.hpp"
+FILE="semver.hpp"
 
 # Download the release
-if [ ! -f "$ZIP" ]; then
-  echo "Downloading $ZIP from $URL ..."
-  curl -L "$URL" -o "$ZIP"
+if [ ! -f "$FILE" ]; then
+  echo "Downloading $FILE from $URL ..."
+  curl -L "$URL" -o "$FILE"
   echo ""
 fi
-
-# Unzip the release
-if [ ! -d "$DIR" ]; then
-  echo "Unzipping $ZIP to .build/$DIR ..."
-  cp "$ZIP" "$ZIP.bak"
-  unzip -q "$ZIP"
-  rm "$ZIP"
-  mv "$ZIP.bak" "$ZIP"
-  echo ""
-fi
-cd ..
-
-# Copy the libs to the package directory
-echo "Copying libs to taskflow/ ..."
-rm -rf taskflow
-mkdir -p taskflow
-cp -rf ".build/$DIR/taskflow/"* taskflow/
-echo ""
 }
 
 
 # Test the project
 test() {
-echo "Running 01-simple ..."
-clang++ -std=c++20 -I. -o 01.exe examples/01-simple.cxx   && ./01.exe && echo -e "\n"
-echo "Running 02-pipeline ..."
-clang++ -std=c++20 -I. -o 02.exe examples/02-pipeline.cxx && ./02.exe && echo -e "\n"
+echo "Running 01-scope_exit.cxx ..."
+g++ -I. -o 01.exe examples/01-scope_exit.cxx     && ./01.exe && echo -e "\n"
+echo "Running 02-scope_fail.cxx ..."
+g++ -I. -o 02.exe examples/02-scope_fail.cxx     && ./02.exe && echo -e "\n"
+echo "Running 03-scope_success.cxx ..."
+g++ -I. -o 03.exe examples/03-scope_success.cxx  && ./03.exe && echo -e "\n"
 }
 
 
